cf1b8a0d9bfc4b9e337a43fc3aea88beb6b5b4fd
TacticsBadger <97360436+TacticsBadger@users.noreply.github.com>
2022-01-08 16:45:43 -0500

    Update MC_MatchSimSimple.py

--- MC_MatchSimSimple.py
+++ MC_MatchSimSimple.py
@@ -12,7 +12,6 @@ import pandas as pd
 import numpy as np
 from prettytable import PrettyTable
 
-
 print("***************** Tactics Not Antics ******************")
 print("*****          Monte Carlo Match Simulator        *****")
 print("*****        Version 1.0: November 17, 2021       *****")
@@ -101,86 +100,61 @@ if choice == "Keyboard":
     sim_table_stats.add_row([num_simulations, round(tot_sim_time,3), count_home_wins, count_away_wins, count_draws])
     sim_table_stats.add_row(["-", "-", str(home_win_probability)+"%", str(away_win_probability)+"%", str(draw_probability)+"%"])
     print(sim_table_stats)
-    # get the score matrix
-    # there has to be an easier way of doing this but it's late and i'm tired so this will do
-    total_scores = len(score_mat)
-    count00 = 0
-    count01 = 0
-    count02 = 0
-    count03 = 0
-    count04 = 0
-    count10 = 0
-    count11 = 0
-    count12 = 0
-    count13 = 0
-    count14 = 0
-    count20 = 0
-    count21 = 0
-    count22 = 0
-    count23 = 0
-    count24 = 0
-    count30 = 0
-    count31 = 0
-    count32 = 0
-    count33 = 0
-    count34 = 0
-    count40 = 0
-    count41 = 0
-    count42 = 0
-    count43 = 0
-    count44 = 0
     
+    # get the score matrix
+    max_score = 5
+    assemble_scores = [[0 for x in range(max_score)] for y in range(max_score)]
     for i in range(total_scores):
         if score_mat[i][0] == 0 and score_mat[i][1] == 0:
-            count00 += 1
+            assemble_scores[0][0] += 1
         elif score_mat[i][0] == 0 and score_mat[i][1] == 1:
-            count01 += 1
+            assemble_scores[0][1] += 1
         elif score_mat[i][0] == 0 and score_mat[i][1] == 2:
-            count02 += 1     
+            assemble_scores[0][2] += 1     
         elif score_mat[i][0] == 0 and score_mat[i][1] == 3:
-            count03 += 1     
+            assemble_scores[0][3] += 1     
         elif score_mat[i][0] == 0 and score_mat[i][1] == 4:
-            count04 += 1    
+            assemble_scores[0][4] += 1    
         elif score_mat[i][0] == 1 and score_mat[i][1] == 0:
-            count10 += 1
+            assemble_scores[1][0] += 1
         elif score_mat[i][0] == 1 and score_mat[i][1] == 1:
-            count11 += 1     
+            assemble_scores[1][1] += 1     
         elif score_mat[i][0] == 1 and score_mat[i][1] == 2:
-            count12 += 1     
+            assemble_scores[1][2] += 1     
         elif score_mat[i][0] == 1 and score_mat[i][1] == 3:
-            count13 += 1     
+            assemble_scores[1][3] += 1     
         elif score_mat[i][0] == 1 and score_mat[i][1] == 4:
-            count14 += 1
+            assemble_scores[1][4] += 1
         elif score_mat[i][0] == 2 and score_mat[i][1] == 0:
-            count20 += 1
+            assemble_scores[2][0] += 1
         elif score_mat[i][0] == 2 and score_mat[i][1] == 1:
-            count21 += 1     
+            assemble_scores[2][1] += 1     
         elif score_mat[i][0] == 2 and score_mat[i][1] == 2:
-            count22 += 1     
+            assemble_scores[2][2] += 1     
         elif score_mat[i][0] == 2 and score_mat[i][1] == 3:
-            count23 += 1     
+            assemble_scores[2][3] += 1     
         elif score_mat[i][0] == 2 and score_mat[i][1] == 4:
-            count24 += 1
+            assemble_scores[2][4] += 1
         elif score_mat[i][0] == 3 and score_mat[i][1] == 0:
-            count30 += 1
+            assemble_scores[3][0] += 1
         elif score_mat[i][0] == 3 and score_mat[i][1] == 1:
-            count31 += 1     
+            assemble_scores[3][1] += 1     
         elif score_mat[i][0] == 3 and score_mat[i][1] == 2:
-            count32 += 1     
+            assemble_scores[3][2] += 1     
         elif score_mat[i][0] == 3 and score_mat[i][1] == 3:
-            count33 += 1     
+            assemble_scores[3][3] += 1     
         elif score_mat[i][0] == 3 and score_mat[i][1] == 4:
-            count34 += 1            
+            assemble_scores[3][4] += 1            
         elif score_mat[i][0] == 4 and score_mat[i][1] == 0:
-            count40 += 1
+            assemble_scores[4][0] += 1
         elif score_mat[i][0] == 4 and score_mat[i][1] == 1:
-            count41 += 1     
+            assemble_scores[4][1] += 1     
         elif score_mat[i][0] == 4 and score_mat[i][1] == 2:
-            count42 += 1     
+            assemble_scores[4][2] += 1     
         elif score_mat[i][0] == 4 and score_mat[i][1] == 3:
-            count43 += 1     
+            assemble_scores[4][3] += 1     
         elif score_mat[i][0] == 4 and score_mat[i][1] == 4:
-            count44 += 1
+            assemble_scores[4][4] += 1    
             
     #calculate percentages and print the score matrix
     print ("**********************************")        
@@ -188,13 +162,13 @@ if choice == "Keyboard":
     print ("*  SCORE MATRIX (% PROBABILITY)  *")
     print ("*                                *")
     print ("**********************************")
-    score_matrix = PrettyTable([" ", 0, 1, 2,3,4])
-    score_matrix.add_row([0,round(count00/num_simulations*100,2),round(count01/num_simulations*100,2),round(count02/num_simulations*100,2),round(count03/num_simulations*100,2),round(count04/num_simulations*100,2)])
-    score_matrix.add_row([1,round(count10/num_simulations*100,2),round(count11/num_simulations*100,2),round(count12/num_simulations*100,2),round(count13/num_simulations*100,2),round(count14/num_simulations*100,2)])
-    score_matrix.add_row([2,round(count20/num_simulations*100,2),round(count21/num_simulations*100,2),round(count22/num_simulations*100,2),round(count23/num_simulations*100,2),round(count24/num_simulations*100,2)])
-    score_matrix.add_row([3,round(count30/num_simulations*100,2),round(count31/num_simulations*100,2),round(count32/num_simulations*100,2),round(count33/num_simulations*100,2),round(count34/num_simulations*100,2)])
-    score_matrix.add_row([4,round(count40/num_simulations*100,2),round(count41/num_simulations*100,2),round(count42/num_simulations*100,2),round(count43/num_simulations*100,2),round(count44/num_simulations*100,2)])
-    print(score_matrix)
+    score_matrix = PrettyTable([" ", 0, 1, 2, 3, 4])
+    score_matrix.add_row([0,round(assemble_scores[0][0]/num_simulations*100,2),round(assemble_scores[0][1]/num_simulations*100,2),round(assemble_scores[0][2]/num_simulations*100,2),round(assemble_scores[0][3]/num_simulations*100,2),round(assemble_scores[0][4]/num_simulations*100,2)])
+    score_matrix.add_row([1,round(assemble_scores[1][0]/num_simulations*100,2),round(assemble_scores[1][1]/num_simulations*100,2),round(assemble_scores[1][2]/num_simulations*100,2),round(assemble_scores[1][3]/num_simulations*100,2),round(assemble_scores[1][4]/num_simulations*100,2)])
+    score_matrix.add_row([2,round(assemble_scores[2][0]/num_simulations*100,2),round(assemble_scores[2][1]/num_simulations*100,2),round(assemble_scores[2][2]/num_simulations*100,2),round(assemble_scores[2][3]/num_simulations*100,2),round(assemble_scores[2][4]/num_simulations*100,2)])
+    score_matrix.add_row([3,round(assemble_scores[3][0]/num_simulations*100,2),round(assemble_scores[3][1]/num_simulations*100,2),round(assemble_scores[3][2]/num_simulations*100,2),round(assemble_scores[3][3]/num_simulations*100,2),round(assemble_scores[3][4]/num_simulations*100,2)])
+    score_matrix.add_row([4,round(assemble_scores[4][0]/num_simulations*100,2),round(assemble_scores[4][1]/num_simulations*100,2),round(assemble_scores[4][2]/num_simulations*100,2),round(assemble_scores[4][3]/num_simulations*100,2),round(assemble_scores[4][4]/num_simulations*100,2)])
+    print(score_matrix) 
     
     #calculate expected Pts and print a summary
     home_xPts = (home_win_probability / 100) * 3.0 + (draw_probability / 100) * 1.0 + (away_win_probability / 100) * 0.0
@@ -207,6 +181,7 @@ if choice == "Keyboard":
     print(input_home_team, "win probability %:", home_win_probability, "xPts =", round(home_xPts,2))
     print(input_away_team, "win probability %:", away_win_probability, "xPts =", round(away_xPts,2))
     print("Draw probability %:", draw_probability)
+    
 elif choice == "csv":
     print ("Reading data from csv file.")
     csv_filename = input("* Add csv filename: ")
@@ -287,100 +262,75 @@ elif choice == "csv":
         sim_table_stats.add_row([num_simulations, round(tot_sim_time,3), count_home_wins, count_away_wins, count_draws])
         sim_table_stats.add_row(["-", "-", str(home_win_probability)+"%", str(away_win_probability)+"%", str(draw_probability)+"%"])
         print(sim_table_stats)
-        # get the score matrix
-        # there has to be an easier way of doing this but it's late and i'm tired so this will do
-        total_scores = len(score_mat)
-        count00 = 0
-        count01 = 0
-        count02 = 0
-        count03 = 0
-        count04 = 0
-        count10 = 0
-        count11 = 0
-        count12 = 0
-        count13 = 0
-        count14 = 0
-        count20 = 0
-        count21 = 0
-        count22 = 0
-        count23 = 0
-        count24 = 0
-        count30 = 0
-        count31 = 0
-        count32 = 0
-        count33 = 0
-        count34 = 0
-        count40 = 0
-        count41 = 0
-        count42 = 0
-        count43 = 0
-        count44 = 0
         
+        # get the score matrix
+        max_score = 5
+        assemble_scores = [[0 for x in range(max_score)] for y in range(max_score)]
         for i in range(total_scores):
-            if score_mat[i][0] == 0 and score_mat[i][1] == 0:
-                count00 += 1
-            elif score_mat[i][0] == 0 and score_mat[i][1] == 1:
-                count01 += 1
-            elif score_mat[i][0] == 0 and score_mat[i][1] == 2:
-                count02 += 1     
-            elif score_mat[i][0] == 0 and score_mat[i][1] == 3:
-                count03 += 1     
-            elif score_mat[i][0] == 0 and score_mat[i][1] == 4:
-                count04 += 1    
-            elif score_mat[i][0] == 1 and score_mat[i][1] == 0:
-                count10 += 1
-            elif score_mat[i][0] == 1 and score_mat[i][1] == 1:
-                count11 += 1     
-            elif score_mat[i][0] == 1 and score_mat[i][1] == 2:
-                count12 += 1     
-            elif score_mat[i][0] == 1 and score_mat[i][1] == 3:
-                count13 += 1     
-            elif score_mat[i][0] == 1 and score_mat[i][1] == 4:
-                count14 += 1
-            elif score_mat[i][0] == 2 and score_mat[i][1] == 0:
-                count20 += 1
-            elif score_mat[i][0] == 2 and score_mat[i][1] == 1:
-                count21 += 1     
-            elif score_mat[i][0] == 2 and score_mat[i][1] == 2:
-                count22 += 1     
-            elif score_mat[i][0] == 2 and score_mat[i][1] == 3:
-                count23 += 1     
-            elif score_mat[i][0] == 2 and score_mat[i][1] == 4:
-                count24 += 1
-            elif score_mat[i][0] == 3 and score_mat[i][1] == 0:
-                count30 += 1
-            elif score_mat[i][0] == 3 and score_mat[i][1] == 1:
-                count31 += 1     
-            elif score_mat[i][0] == 3 and score_mat[i][1] == 2:
-                count32 += 1     
-            elif score_mat[i][0] == 3 and score_mat[i][1] == 3:
-                count33 += 1     
-            elif score_mat[i][0] == 3 and score_mat[i][1] == 4:
-                count34 += 1            
-            elif score_mat[i][0] == 4 and score_mat[i][1] == 0:
-                count40 += 1
-            elif score_mat[i][0] == 4 and score_mat[i][1] == 1:
-                count41 += 1     
-            elif score_mat[i][0] == 4 and score_mat[i][1] == 2:
-                count42 += 1     
-            elif score_mat[i][0] == 4 and score_mat[i][1] == 3:
-                count43 += 1     
-            elif score_mat[i][0] == 4 and score_mat[i][1] == 4:
-                count44 += 1
-                
+        if score_mat[i][0] == 0 and score_mat[i][1] == 0:
+            assemble_scores[0][0] += 1
+        elif score_mat[i][0] == 0 and score_mat[i][1] == 1:
+            assemble_scores[0][1] += 1
+        elif score_mat[i][0] == 0 and score_mat[i][1] == 2:
+            assemble_scores[0][2] += 1     
+        elif score_mat[i][0] == 0 and score_mat[i][1] == 3:
+            assemble_scores[0][3] += 1     
+        elif score_mat[i][0] == 0 and score_mat[i][1] == 4:
+            assemble_scores[0][4] += 1    
+        elif score_mat[i][0] == 1 and score_mat[i][1] == 0:
+            assemble_scores[1][0] += 1
+        elif score_mat[i][0] == 1 and score_mat[i][1] == 1:
+            assemble_scores[1][1] += 1     
+        elif score_mat[i][0] == 1 and score_mat[i][1] == 2:
+            assemble_scores[1][2] += 1     
+        elif score_mat[i][0] == 1 and score_mat[i][1] == 3:
+            assemble_scores[1][3] += 1     
+        elif score_mat[i][0] == 1 and score_mat[i][1] == 4:
+            assemble_scores[1][4] += 1
+        elif score_mat[i][0] == 2 and score_mat[i][1] == 0:
+            assemble_scores[2][0] += 1
+        elif score_mat[i][0] == 2 and score_mat[i][1] == 1:
+            assemble_scores[2][1] += 1     
+        elif score_mat[i][0] == 2 and score_mat[i][1] == 2:
+            assemble_scores[2][2] += 1     
+        elif score_mat[i][0] == 2 and score_mat[i][1] == 3:
+            assemble_scores[2][3] += 1     
+        elif score_mat[i][0] == 2 and score_mat[i][1] == 4:
+            assemble_scores[2][4] += 1
+        elif score_mat[i][0] == 3 and score_mat[i][1] == 0:
+            assemble_scores[3][0] += 1
+        elif score_mat[i][0] == 3 and score_mat[i][1] == 1:
+            assemble_scores[3][1] += 1     
+        elif score_mat[i][0] == 3 and score_mat[i][1] == 2:
+            assemble_scores[3][2] += 1     
+        elif score_mat[i][0] == 3 and score_mat[i][1] == 3:
+            assemble_scores[3][3] += 1     
+        elif score_mat[i][0] == 3 and score_mat[i][1] == 4:
+            assemble_scores[3][4] += 1            
+        elif score_mat[i][0] == 4 and score_mat[i][1] == 0:
+            assemble_scores[4][0] += 1
+        elif score_mat[i][0] == 4 and score_mat[i][1] == 1:
+            assemble_scores[4][1] += 1     
+        elif score_mat[i][0] == 4 and score_mat[i][1] == 2:
+            assemble_scores[4][2] += 1     
+        elif score_mat[i][0] == 4 and score_mat[i][1] == 3:
+            assemble_scores[4][3] += 1     
+        elif score_mat[i][0] == 4 and score_mat[i][1] == 4:
+            assemble_scores[4][4] += 1     
+            
         #calculate percentages and print the score matrix
         print ("**********************************")        
         print ("*                                *")       
         print ("*  SCORE MATRIX (% PROBABILITY)  *")
         print ("*                                *")
         print ("**********************************")
-        score_matrix = PrettyTable([" ", 0, 1, 2,3,4])
-        score_matrix.add_row([0,round(count00/num_simulations*100,2),round(count01/num_simulations*100,2),round(count02/num_simulations*100,2),round(count03/num_simulations*100,2),round(count04/num_simulations*100,2)])
-        score_matrix.add_row([1,round(count10/num_simulations*100,2),round(count11/num_simulations*100,2),round(count12/num_simulations*100,2),round(count13/num_simulations*100,2),round(count14/num_simulations*100,2)])
-        score_matrix.add_row([2,round(count20/num_simulations*100,2),round(count21/num_simulations*100,2),round(count22/num_simulations*100,2),round(count23/num_simulations*100,2),round(count24/num_simulations*100,2)])
-        score_matrix.add_row([3,round(count30/num_simulations*100,2),round(count31/num_simulations*100,2),round(count32/num_simulations*100,2),round(count33/num_simulations*100,2),round(count34/num_simulations*100,2)])
-        score_matrix.add_row([4,round(count40/num_simulations*100,2),round(count41/num_simulations*100,2),round(count42/num_simulations*100,2),round(count43/num_simulations*100,2),round(count44/num_simulations*100,2)])
-        print(score_matrix)
+        score_matrix = PrettyTable([" ", 0, 1, 2, 3, 4])
+        score_matrix.add_row([0,round(assemble_scores[0][0]/num_simulations*100,2),round(assemble_scores[0][1]/num_simulations*100,2),round(assemble_scores[0][2]/num_simulations*100,2),round(assemble_scores[0][3]/num_simulations*100,2),round(assemble_scores[0][4]/num_simulations*100,2)])
+        score_matrix.add_row([1,round(assemble_scores[1][0]/num_simulations*100,2),round(assemble_scores[1][1]/num_simulations*100,2),round(assemble_scores[1][2]/num_simulations*100,2),round(assemble_scores[1][3]/num_simulations*100,2),round(assemble_scores[1][4]/num_simulations*100,2)])
+        score_matrix.add_row([2,round(assemble_scores[2][0]/num_simulations*100,2),round(assemble_scores[2][1]/num_simulations*100,2),round(assemble_scores[2][2]/num_simulations*100,2),round(assemble_scores[2][3]/num_simulations*100,2),round(assemble_scores[2][4]/num_simulations*100,2)])
+        score_matrix.add_row([3,round(assemble_scores[3][0]/num_simulations*100,2),round(assemble_scores[3][1]/num_simulations*100,2),round(assemble_scores[3][2]/num_simulations*100,2),round(assemble_scores[3][3]/num_simulations*100,2),round(assemble_scores[3][4]/num_simulations*100,2)])
+        score_matrix.add_row([4,round(assemble_scores[4][0]/num_simulations*100,2),round(assemble_scores[4][1]/num_simulations*100,2),round(assemble_scores[4][2]/num_simulations*100,2),round(assemble_scores[4][3]/num_simulations*100,2),round(assemble_scores[4][4]/num_simulations*100,2)])
+        print(score_matrix) 
         
         #calculate expected Pts and print a summary
         home_xPts = (home_win_probability / 100) * 3.0 + (draw_probability / 100) * 1.0 + (away_win_probability / 100) * 0.0
